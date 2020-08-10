b61c747e6206fdae727d46292dde5196051ff09e
Pratik Dhumal <68268541+pratikd2124@users.noreply.github.com>
2020-08-10 19:48:28 +0530

    whatsapp automated message in python

--- whatsappauto.py
+++ whatsappauto.py
@@ -1,7 +1,7 @@
 import pywhatkit
 
-pywhatkit.sendwhatmsg("+919876543210","This is a message is sent by whatsapp automation",15,00)#Will open web.whatsapp.com at 14:59 and message will be sent at exactly 15:00
+pywhatkit.sendwhatmsg("+919976543220","This is a message is sent by whatsapp automation",15,00)#Will open web.whatsapp.com at 14:59 and message will be sent at exactly 15:00
 
-#pywhatkit.sendwhatmsg_with_selenium("+919876543210","This is a message",15,00)#Will send message with most of the processes hidden
+#pywhatkit.sendwhatmsg_with_selenium("+919976543220","This is a message",15,00)#Will send message with most of the processes hidden
 
 #pywhatkit.help() #For more information
